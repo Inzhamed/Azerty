d5b9bde822ae3c00bc0603f82cde6d352c4752e7
ferielkacel <121347180+ferielkacel@users.noreply.github.com>
2024-03-02 07:12:13 +0100

    Delete backend_enola directory

--- backend_enola/routers/gemini.js
+++
@@ -1,8 +0,0 @@
-// router.js
-const express = require("express");
-const router = express.Router();
-const { getAnswer } = require("./controller");
-
-router.post("/getAnswer", getAnswer);
-
-module.exports = router;
