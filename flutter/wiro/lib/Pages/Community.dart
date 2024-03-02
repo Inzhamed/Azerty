88598a678f987fbfb1e0c868ad828cf3300e0022
Naaylla <96737235+Naaylla@users.noreply.github.com>
2024-03-02 06:39:40 +0100

    commit change to community
    this is a call for help, im on caffeine overdose

--- flutter/wiro/lib/Pages/Community.dart
+++ flutter/wiro/lib/Pages/Community.dart
@@ -1,50 +1,112 @@
 import 'package:flutter/material.dart';
 
+const TextStyle customTextStyle = TextStyle(
+  color: Color(0xff140562),
+  fontSize: 14,
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+);
+
 class CommunityClass extends StatelessWidget {
   const CommunityClass({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("debug"),
-        ),
-      ),
+      home: Scaffold(body: TabBarExample()),
     );
   }
 }
 
-class TabBarApp extends StatelessWidget {
-  const TabBarApp({super.key});
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      initialIndex: 1,
+      initialIndex: 2,
       length: 3,
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: const Text('Lotus'),
+          title: const Text('Lotus',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w700,
+                fontSize: 28,
+                color: Color(0xff140562),
+              )),
           bottom: TabBar(
             tabs: <Widget>[
-              Tab(text: "Explore"),
-              Tab(text: "For You"),
-              Tab(text: "Groups"),
+              Tab(
+                child: (Text("Explore", style: customTextStyle)),
+              ),
+              Tab(
+                child: (Text("For You", style: customTextStyle)),
+              ),
+              Tab(
+                child: (Text("Groups", style: customTextStyle)),
+              ),
             ],
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
             Center(
-              child: Text("It's cloudy here"),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Container(
+                          width: 200,
+                          child: const Center(
+                              child: Text(
+                            'Item 1',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )),
+                        ),
+                        Container(
+                          width: 200,
+                          child: const Center(
+                              child: Text(
+                            'Item 2',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )),
+                        ),
+                        Container(
+                          width: 200,
+                          child: const Center(
+                              child: Text(
+                            'Item 3',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )),
+                        ),
+                        Container(
+                          width: 200,
+                          color: Colors.purple[300],
+                          child: const Center(
+                              child: Text(
+                            'Item 4',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
             Center(
-              child: Text("It's rainy here"),
+              child: Row(
+                children: [],
+              ),
             ),
             Center(
-              child: Text("It's sunny here"),
+              child: Row(
+                children: [],
+              ),
             ),
           ],
         ),
